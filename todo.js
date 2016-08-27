d972981aaa593044f90a16e48170c893d01e0ebd
Ridwan A. Otun <readonegoke@gmail.com>
2016-08-27 14:53:20 +0100

    added function to display todo

--- todo.js
+++ todo.js
@@ -1,5 +1,10 @@
 var todos = ['item 1', 'item 2', 'item 3']
 
+//function to display todos
+function displayTodos() {
+  console.log('My todos: ', todos);
+}
+
 //function to add todos.
 function addTodo(todo) {
   todos.push(todo);
